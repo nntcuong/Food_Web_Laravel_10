d0b4179df34e20f6e23096512f1199799b3d061b
nntcuong <thecuongyt3htk@gmail.com>
2024-08-20 07:47:21 +0700

    'dfdf'

--- app/DataTables/SliderDataTable.php
+++ app/DataTables/SliderDataTable.php
@@ -26,7 +26,16 @@ class SliderDataTable extends DataTable
                 $edit = "<a href='".route('admin.slider.edit',$query->id)."' class='btn btn-primary'><i class='fas fa-edit'></i></a>";
                 $delete="<a href='".route('admin.slider.destroy',$query->id)."' class='btn btn-danger delete-item ml-2'><i class='fas fa-trash'></i></a>";
                 return $edit.$delete;
+            })->addColumn('image',function($query){
+                return '<img width="150px" .src="'.asset($query->image).'">';
+            })->addColumn('status',function($query){
+                if($query->status===1){
+                    return '<span class="badge badge-primary">Active</span>';
+                }else{
+                    return '<span class="badge badge-danger">InActive</span>';
+                }
             })
+            ->rawColumns(['image', 'action', 'status'])
             ->setRowId('id');
     }
 
@@ -48,7 +57,7 @@ class SliderDataTable extends DataTable
                     ->columns($this->getColumns())
                     ->minifiedAjax()
                     //->dom('Bfrtip')
-                    ->orderBy(1)
+                    ->orderBy(0)
                     ->selectStyleSingle()
                     ->buttons([
                         Button::make('excel'),
@@ -66,10 +75,10 @@ class SliderDataTable extends DataTable
     public function getColumns(): array
     {
         return [
-            Column::make('id'),
-            Column::make('image'),
+            Column::make('id')->width(60),
+            Column::make('image')->width(150),
             Column::make('title'),
-            //Column::make('action'),
+            Column::make('status'),
             Column::computed('action')
                   ->exportable(false)
                   ->printable(false)
